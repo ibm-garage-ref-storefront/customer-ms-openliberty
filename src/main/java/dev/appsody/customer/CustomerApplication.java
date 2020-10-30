2580f99a474d0417a8729769d0d4220ab4828ea8
Hemankita Perabathini <Hemankita1@Hemankitas-MacBook-Pro-2.local>
2020-10-30 16:04:53 +0530

    changing the template defaults

---
+++ src/main/java/dev/appsody/customer/CustomerApplication.java
@@ -0,0 +1,9 @@
+package dev.appsody.customer;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/micro")
+public class CustomerApplication extends Application {
+
+}
