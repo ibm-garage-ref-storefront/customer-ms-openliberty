afb0c52072f71bfb94e1680cf5fdf37a50d140e7
Hemankita Perabathini <Hemankita1@Hemankitas-MacBook-Pro-2.local>
2020-11-02 16:03:52 +0530

    customer apis

--- src/main/java/dev/appsody/customer/CustomerApplication.java
+++ src/main/java/dev/appsody/customer/CustomerApplication.java
@@ -1,8 +1,13 @@
 package dev.appsody.customer;
 
+import javax.annotation.security.DeclareRoles;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import org.eclipse.microprofile.auth.LoginConfig;
+
+@LoginConfig(authMethod = "MP-JWT")
+@DeclareRoles({"admin", "user"})
 @ApplicationPath("/micro")
 public class CustomerApplication extends Application {
 
---
+++ src/main/java/dev/appsody/customer/model/Customer.java
@@ -0,0 +1,68 @@
+package dev.appsody.customer.model;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema(name="Customer", description="POJO that represents the items of the customer")
+public class Customer {
+	
+	private String _id;
+	private String _rev;
+	private String username;
+    private String password;
+    private String firstName;
+	private String lastName;
+	private String email;
+	private String imageUrl;
+	
+	public String get_id() {
+		return _id;
+	}
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+	public String get_rev() {
+		return _rev;
+	}
+	public void set_rev(String _rev) {
+		this._rev = _rev;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+	
+
+}
